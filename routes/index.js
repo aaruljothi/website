e343926063ed843e9ee432e8a2a7b6d3b8021ce7
Arun Aruljothi <arun3454@gmail.com>
2018-01-26 22:37:58 -0500

    cover and error update basic templating for everything else built

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('cover');
+});
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 module.exports = router;
